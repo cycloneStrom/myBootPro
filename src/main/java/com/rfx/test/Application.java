09ee38301b67e0ef78b386395feaa4e1fba4f945
cycloneStrom <renfaxin@qq.com>
2018-02-07 09:43:02 +0800

    spring boot

---
+++ src/main/java/com/rfx/test/Application.java
@@ -0,0 +1,15 @@
+package com.rfx.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@ComponentScan
+@PropertySource("application.properties")
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
